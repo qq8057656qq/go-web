753949f364c633be940ace88bf476e850c5fe8be
leo <lvhuan2015@gmail.com>
2022-07-18 22:00:45 +0800

    init

---
+++ gee/gee.go
@@ -0,0 +1,47 @@
+package gee
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+//constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "404 NOT FOUND : %s\n", req.URL)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,4 @@
+module example
+go 1.23
+require gee v0.0.0
+replace gee => ./gee
